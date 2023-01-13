4a79cf1d1c18633656cf1e6eae2270b48d407ce7
Flavia <flavia.bulad@gmail.com>
2023-01-13 15:09:42 -0300

    add solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Holidays holidays = new Holidays();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nDigite uma data no formato dd/mm/aaaa ");
+        String date = input.nextLine();
+
+        holidays.isHoliday(date);
+
+        System.out.println("\nVeja abaixo uma lista com todos os feriados brasileiros em 2023:");
+        holidays.printHolidays();
+      
+
     }
 }
